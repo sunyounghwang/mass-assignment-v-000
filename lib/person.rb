ec4cde961f17261e4506d65967d7a9fe61d1d68c
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-23 17:02:03 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,6 +2,6 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-    attributes.each { |key, value| send.  }
+    attributes.each { |key, value| send. }
   end
 end
